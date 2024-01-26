93b378652dde8a55ac29816eeab30d47001a6fc4
Felipe Antero <souzantero@gmail.com>
2024-01-25 21:55:52 -0300

    add unit tests

---
+++ src/core/application/use-cases/find-customer.test.ts
@@ -0,0 +1,13 @@
+import { CustomerInMemoryDatabase } from "../../../main/databases/in-memory/customer-in-memory-database";
+import { FindCustomer } from "./find-customer";
+
+describe('FindCustomer', () => {
+  describe('findOneByDocument', () => {
+    it('should be null', () => {
+      const customerRepository = new CustomerInMemoryDatabase();
+      const findCustomer = new FindCustomer(customerRepository);
+      const customer = findCustomer.findOneByDocument('12345678901');
+      expect(customer).toBeNull();
+    })
+  })
+})
